dcb9c71c017ea423134d76ada74c9524c2ac29e0
Dmitriy Filippov <Amoralis.19@gmail.com>
2020-01-23 20:50:16 +0300

    1

--- js/common.js
+++ js/common.js
@@ -1,3 +1,29 @@
 $(document).ready(function () {
-  
+  if ($(window).width() > 992) {
+    $('.rec-left').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+      arrows: false
+    });
+  } else {
+    $('.rec-left').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+      arrows: false
+    });
+  }
+
+ 
+    $('.feed-wrap-slider').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+      arrows: true
+    });
+ 
 });

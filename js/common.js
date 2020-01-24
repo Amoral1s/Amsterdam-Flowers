4c7856355a1305d1db0993539707ea67e8dc4541
Dmitriy Filippov <Amoralis.19@gmail.com>
2020-01-24 22:01:31 +0300

    1

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  if ($(window).width() > 992) {
+  if ($(window).width() > 768) {
     $('.rec-left').slick({
       slidesToShow: 3,
       slidesToScroll: 1,
@@ -7,7 +7,16 @@ $(document).ready(function () {
       dots: true,
       arrows: false
     });
-  } else {
+  } else if ($(window).width() > 578){
+    $('.rec-left').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+      arrows: false
+    });
+  }
+   else {
     $('.rec-left').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
@@ -17,7 +26,7 @@ $(document).ready(function () {
     });
   }
 
- 
+  if ($(window).width() > 992) {
     $('.feed-wrap-slider').slick({
       slidesToShow: 2,
       slidesToScroll: 1,
@@ -25,5 +34,21 @@ $(document).ready(function () {
       dots: true,
       arrows: true
     });
- 
+  } else {
+    $('.feed-wrap-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+      arrows: false
+    });
+  }
+
+
+
+    $('.burger').on('click', () => {
+      $('.burger').toggleClass('burger-active');
+      $('.header-bot-wrap-menu').slideToggle(600);
+      $('.header-top').fadeToggle(600);
+    });
 });
